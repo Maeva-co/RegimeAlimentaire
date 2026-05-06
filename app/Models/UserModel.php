1a644946b1fc9d3eedabca80b3f9c17724dd0936
brandon0638 <brandonandria06@gmail.com>
2026-05-06 21:41:26 +0200

    user controller

---
+++ app/Models/UserModel.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table = 'User';
+    protected $primaryKey = 'id';
+    protected $allowedFields = [
+        'nom', 'email', 'password', 'genre', 
+        'taille', 'poids', 'IMC', 'balance', 'role'
+    ];
+    
+    protected $useTimestamps = true;
+    protected $createdField = 'created_at';
+    protected $updatedField = null;
+    
+    protected $validationRules = [
+        'nom' => 'required|min_length[2]',
+        'email' => 'required|valid_email|is_unique[User.email,id,{id}]',
+        'password' => 'required|min_length[3]'
+    ];
+    
+    protected $validationMessages = [
+        'nom' => [
+            'required' => 'Le nom est obligatoire',
+            'min_length' => 'Le nom doit contenir au moins 2 caractères'
+        ],
+        'email' => [
+            'required' => 'L\'email est obligatoire',
+            'valid_email' => 'Email invalide',
+            'is_unique' => 'Cet email est déjà utilisé'
+        ],
+        'password' => [
+            'required' => 'Le mot de passe est obligatoire',
+            'min_length' => 'Le mot de passe doit contenir au moins 3 caractères'
+        ]
+    ];
+}
