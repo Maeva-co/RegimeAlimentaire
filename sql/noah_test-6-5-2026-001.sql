668ae54e1233cb59fcfda2e03557245704869d15
Noah Andria <noahandriamanana41@.com>
2026-05-08 11:38:46 +0300

    resolve conflict

---
+++ sql/noah_test-6-5-2026-001.sql
@@ -0,0 +1,8 @@
+insert into User values (1, "Noah", "noah@example.com", "M", 180, 75, 23.15, 1000.00);
+insert into Code values (1, "cd1", 100);
+insert into Code values (2, "cd2", 200);
+insert into Code values (3, "cd3", 250);
+insert into Code values (4, "cd4", 90);
+insert into Code values (5, "cd5", 10);
+
+insert into Option values (1, "Gold", 500, 15);
