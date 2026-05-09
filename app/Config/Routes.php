1d7988dff7439da9d65624e2b340fa4f787bdd8f
brandon0638 <brandonandria06@gmail.com>
2026-05-09 20:43:17 +0200

    modification de route pour admin

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -22,9 +22,36 @@ $routes->post('/code/redeem', 'CodeController::redeem');
 
 // Back Office Admin
 $routes->group('admin', ['filter' => 'role:admin'], function($routes) {
+    // Dashboard
     $routes->get('dashboard', 'Admin\DashboardController::index');
-    $routes->resource('regimes', ['controller' => 'Admin\RegimeController']);
-    $routes->resource('sports', ['controller' => 'Admin\SportController']);
-    $routes->resource('codes', ['controller' => 'Admin\CodeController']);
-    $routes->resource('parametres', ['controller' => 'Admin\ParametreController']);
+    $routes->get('dashboard/stats', 'Admin\DashboardController::getStats');
+    
+    // CRUD Régimes
+    $routes->get('regimes', 'Admin\RegimeController::index');
+    $routes->get('regimes/create', 'Admin\RegimeController::create');
+    $routes->post('regimes/store', 'Admin\RegimeController::store');
+    $routes->get('regimes/edit/(:num)', 'Admin\RegimeController::edit/$1');
+    $routes->post('regimes/update/(:num)', 'Admin\RegimeController::update/$1');
+    $routes->get('regimes/delete/(:num)', 'Admin\RegimeController::delete/$1');
+    
+    // CRUD Sports
+    $routes->get('sports', 'Admin\SportController::index');
+    $routes->get('sports/create', 'Admin\SportController::create');
+    $routes->post('sports/store', 'Admin\SportController::store');
+    $routes->get('sports/edit/(:num)', 'Admin\SportController::edit/$1');
+    $routes->post('sports/update/(:num)', 'Admin\SportController::update/$1');
+    $routes->get('sports/delete/(:num)', 'Admin\SportController::delete/$1');
+    
+    // CRUD Codes
+    $routes->get('codes', 'Admin\CodeController::index');
+    $routes->get('codes/create', 'Admin\CodeController::create');
+    $routes->post('codes/store', 'Admin\CodeController::store');
+    $routes->get('codes/edit/(:num)', 'Admin\CodeController::edit/$1');
+    $routes->post('codes/update/(:num)', 'Admin\CodeController::update/$1');
+    $routes->get('codes/delete/(:num)', 'Admin\CodeController::delete/$1');
+    
+    // CRUD Paramètres
+    $routes->get('parametres', 'Admin\ParametreController::index');
+    $routes->get('parametres/edit/(:num)', 'Admin\ParametreController::edit/$1');
+    $routes->post('parametres/update/(:num)', 'Admin\ParametreController::update/$1');
 });
